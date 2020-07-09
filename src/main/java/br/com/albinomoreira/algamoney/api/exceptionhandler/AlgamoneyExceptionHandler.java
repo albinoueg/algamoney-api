9196d8b86dfe3395b29ced11902825a0a128b4ae
Albino Moreira <albino.sistemas@gmail.com>
2020-07-08 21:57:34 -0300

    3.10. Validando valores com Bean Validation
    Adição de denpendencia do spring validation e arquivo
    ValidationMessages.properties

--- src/main/java/br/com/albinomoreira/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/br/com/albinomoreira/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -1,5 +1,10 @@
 package br.com.albinomoreira.algamoney.api.exceptionhandler;
 
+import java.io.FileDescriptor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,6 +12,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -24,7 +32,31 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler{
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.getCause().toString();
 		
-		return super.handleExceptionInternal(ex, new ErrorMessages(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		List<ErrorMessages> erros = Arrays.asList(new ErrorMessages(mensagemUsuario, mensagemDesenvolvedor));
+		
+		return super.handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
 	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		
+		List<ErrorMessages> erros = criarListaDeErros(ex.getBindingResult());
+		
+		return handleExceptionInternal(ex, erros , headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<ErrorMessages> criarListaDeErros(BindingResult bindingResult){
+		List<ErrorMessages> erros = new ArrayList<ErrorMessages>();
+		
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			
+			erros.add(new ErrorMessages(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		
+		return erros;
+	}
 }
