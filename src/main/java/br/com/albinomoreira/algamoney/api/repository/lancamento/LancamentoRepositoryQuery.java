973350c9847cc47f3413f400e1e105d966ec2f26
Albino Moreira <albino.sistemas@gmail.com>
2020-07-10 17:57:28 -0300

    5.9. Implementação da paginação de lançamentos

--- src/main/java/br/com/albinomoreira/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/albinomoreira/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,11 +1,12 @@
 package br.com.albinomoreira.algamoney.api.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.albinomoreira.algamoney.api.model.Lancamento;
 import br.com.albinomoreira.algamoney.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
