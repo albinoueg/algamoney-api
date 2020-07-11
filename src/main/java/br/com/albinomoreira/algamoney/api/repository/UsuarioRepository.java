40485ca1d6a7080da300708042d49ea948b9b527
Albino Moreira <albino.sistemas@gmail.com>
2020-07-11 09:52:32 -0300

    6.11. Atualizando o OAuth2 para utilizar BCrypt
    - Atualização para buscar usuário e senha do banco.
    - Atualização para usar o BCrypt para encodar a senha.

---
+++ src/main/java/br/com/albinomoreira/algamoney/api/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.com.albinomoreira.algamoney.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.albinomoreira.algamoney.api.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByEmail(String email);
+}
